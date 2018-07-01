27a116cf5eebb663986c1fd8f73c6035bd46bc0e
Niti Singhal <gupta.sweet.niti@gmail.com>
2018-07-01 12:39:24 -0700

    my online grocery store

--- script/main.js
+++ script/main.js
@@ -7,21 +7,30 @@ function clickTask (clickContent, navResutls){
   });
 }
 
-function repeatTask (clickContent, navResutls, hideElement){
+function repeatTask (clickContent, navResutls){
   document.getElementById(clickContent).addEventListener("click", function(e){
-    document.getElementById(navResutls).classList.toggle('hidden');
-
+    let hideElement = document.getElementsByClassName('itemi');
     for(let i=0; i<hideElement.length;i++) {
-      document.getElementById(hideElement[i]).classList.add('hidden');
+      hideElement[i].classList.add('hidden');
     }
+
+    document.getElementById(navResutls).classList.toggle('hidden');
 });
 }
 
+// function repeatTask (clickContent, navResutls, hideElement){
+//   document.getElementById(clickContent).addEventListener("click", function(e){
+//     document.getElementById(navResutls).classList.toggle('hidden');
+// for(let i=0; i<hideElement.length;i++) {
+//       document.getElementById(hideElement[i]).classList.add('hidden');
+//     }
+// });
+// }
+
 clickTask('shop-online',['secondary-nav','shop-cart']);
-// repeatTask('shop-online','secondary-nav');
-repeatTask('frui', 'fruit', ['veggies', 'cereal', 'candy', 'juice']);
-repeatTask('veg','veggies', ['fruit', 'cereal', 'candy', 'juice']);
-repeatTask('cer','cereal', ['veggies', 'fruit', 'candy', 'juice']);
-repeatTask('ca','candy', ['veggies', 'cereal', 'fruit', 'juice']);
-repeatTask('jui','juice', ['veggies', 'cereal', 'candy', 'fruit']);
-// repeatTask('frui','fruits');
+
+repeatTask('frui', 'fruit');
+repeatTask('veg','veggies');
+repeatTask('cer','cereal');
+repeatTask('ca','candy');
+repeatTask('jui','juice');
