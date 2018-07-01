33404c81c8ba07f9cd199fa20f7306747823b839
Niti Singhal <gupta.sweet.niti@gmail.com>
2018-07-01 14:20:06 -0700

    my online grocery store

--- script/main.js
+++ script/main.js
@@ -18,19 +18,59 @@ function repeatTask (clickContent, navResutls){
 });
 }
 
-// function repeatTask (clickContent, navResutls, hideElement){
-//   document.getElementById(clickContent).addEventListener("click", function(e){
-//     document.getElementById(navResutls).classList.toggle('hidden');
-// for(let i=0; i<hideElement.length;i++) {
-//       document.getElementById(hideElement[i]).classList.add('hidden');
-//     }
-// });
-// }
-
 clickTask('shop-online',['secondary-nav','shop-cart']);
-
 repeatTask('frui', 'fruit');
 repeatTask('veg','veggies');
 repeatTask('cer','cereal');
 repeatTask('ca','candy');
 repeatTask('jui','juice');
+
+let cart = [];
+let x = document.getElementsByClassName('atc');
+for(let i=0; i<x.length; i++){
+x[i].addEventListener('click',function(e){
+  let buttonName = e.target.getAttribute('data-name');
+  let buttonPrice = e.target.getAttribute('data-price');
+  let item = {
+      name : buttonName,
+      price : buttonPrice
+  };
+  cart.push(item);
+
+  updateCart();
+
+});
+
+}
+
+
+
+function updateCart() {
+  let sum = 0;
+  for(let i = 0; i < cart.length ; i++){
+    let item = cart[i];
+
+     sum = sum + Number(item.price);
+
+  }
+
+  let ul = document.getElementById('cart-items');
+
+  // delete all current items in cart
+  while (ul.firstChild) {
+    ul.removeChild(ul.firstChild);
+  }
+
+  // update items in Cart
+  for(let i = 0; i < cart.length; i++) {
+    let item = cart[i];
+
+    let li = document.createElement('li');
+    li.innerHTML = item.name + ' for $' + item.price;
+
+    ul.appendChild(li);
+  }
+
+  // update the price
+  document.getElementById('cart-total').innerHTML = '$' + sum;
+}
