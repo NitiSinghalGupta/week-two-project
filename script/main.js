988e514df672bd7544eadb17c0dc6f6eb3ee7f23
Niti Singhal <gupta.sweet.niti@gmail.com>
2018-07-01 11:31:53 -0700

    my online grocery store

--- script/main.js
+++ script/main.js
@@ -6,54 +6,9 @@ function repeatTask (clickContent, navResutls){
 }
 
 repeatTask('shop-online','secondary-nav');
-
-document.getElementById('frui').addEventListener("click", function(e){
-  document.getElementById('fruit').classList.remove('hidden');
-});
-
-
-
-document.getElementById('veg').addEventListener("click",displayFruits);
-
-function displayFruits(e){
-  document.getElementById('veggies').classList.remove('hidden');
-}
-
-document.getElementById('cer').addEventListener("click",displayFruits);
-
-function displayFruits(e){
-  document.getElementById('cereal').classList.remove('hidden');
-}
-
-document.getElementById('jui').addEventListener("click",displayFruits);
-
-function displayFruits(e){
-  document.getElementById('juice').classList.remove('hidden');
-}
-
-document.getElementById('ca').addEventListener("click",displayFruits);
-
-function displayFruits(e){
-  document.getElementById('candy').classList.remove('hidden');
-}
-
-
-
-
-
-
-
-// var fruits = [];
-//
-// fruits.push({ name : 'bananas', price : 2.99, image : '/fruits/bananas.png' });
-// fruits.push({ name : 'cherries', price : 8.99, image : '/fruits/cherries.png' });
-//
-// let parent = document.getElementById('fruits');
-// for(let index = 0; index < fruits.length; index++) {
-//   let fruit = fruits[index];
-//   // generate card element
-//   let card = document.createElement('div');
-//   card.addClass('card-item');
-//  // push it to dom
-//   parent.appendChild(card);
-// }
+repeatTask('frui','fruits');
+repeatTask('veg','veggies');
+repeatTask('cer','cereal');
+repeatTask('ca','candy');
+repeatTask('jui','juice');
+repeatTask('frui','fruits');
